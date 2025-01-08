89bfd9c057b8752c261c8dca382179c59c62fb88
yashgarg19 <gargyash9057@gmail.com>
2025-01-08 15:16:57 +0530

    Initial commit: Basic setup for Online Quiz System

---
+++ src/main/java/io/OnlineQuizSystem/OnlineQuizSystemApplication.java
@@ -0,0 +1,13 @@
+package io.OnlineQuizSystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OnlineQuizSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OnlineQuizSystemApplication.class, args);
+	}
+
+}
---
+++ src/test/java/io/OnlineQuizSystem/OnlineQuizSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package io.OnlineQuizSystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OnlineQuizSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
